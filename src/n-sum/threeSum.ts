383eb9047f0e53a4d1c2226557334fece4b5f318
reeli <reeli.null@gmail.com>
2022-01-08 22:50:05 +0800

    add threeSum

--- src/n-sum/threeSum.ts
+++ src/n-sum/threeSum.ts
@@ -1,28 +1,59 @@
 // 15. 三数之和
 // https://leetcode-cn.com/problems/3sum/
+// 用坐标原点来理解，for 循环是在改变坐标原点,l 和 r 分别在坐标原点的两侧。 l--0---r
 
 function threeSum(nums: number[]): number[][] {
-  nums.sort((a, b) => a - b);
-
   const n = nums.length;
+
+  if (!nums || n < 3) {
+    return [];
+  }
+
   const res: number[][] = [];
   const map = new Map();
 
-  if (!nums || n < 3) {
-    return res;
-  }
+  nums.sort((a, b) => a - b);
+
+  let l = 0;
+  let r = 0;
 
   for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-      for (let k = j + 1; k < n; k++) {
-        if (nums[i] + nums[j] + nums[k] == 0) {
-          const list = [nums[i], nums[j], nums[k]];
-          if (map.has(list.join(","))) {
-            continue;
-          }
-          res.push(list);
-          map.set(list.join(","), 1);
+    if (nums[i] > 0) {
+      break;
+    }
+
+    // if (i > 0 && nums[i] === nums[i - 1]) {
+    //   continue;
+    // }
+
+    l = i + 1;
+    r = n - 1;
+
+    while (l < r) {
+      const total = nums[i] + nums[l] + nums[r];
+
+      if (total == 0) {
+        const k = [nums[i], nums[l], nums[r]].join(",");
+        if(!map.has(k)){
+          map.set(k, 1);
+          res.push([nums[i], nums[l], nums[r]]);
         }
+
+        //
+        // while (l < r && nums[l] == nums[l + 1]) {
+        //   l = l + 1;
+        // }
+        //
+        // while (l < r && nums[r] == nums[r - 1]) {
+        //   r = r - 1;
+        // }
+
+        l = l + 1
+        r = r - 1
+      } else if (total > 0) {
+        r = r - 1;
+      } else {
+        l = l + 1;
       }
     }
   }
@@ -30,7 +61,6 @@ function threeSum(nums: number[]): number[][] {
   return res;
 }
 
-console.log(threeSum([-1, 0, 1, 2, -1, -4]), 'xxx')
-// 输入：nums = [-1,0,1,2,-1,-4]
-// 输出：[[-1,-1,2],[-1,0,1]]
-// -4, -1, -1, 0, 1, 2,
+console.log(threeSum([-1, 0, 1, 1,1,1, 2, -1, -4]))
+// console.log(threeSum([0, 0, 0, 0]))
+// console.log(threeSum([-1, 0, 1, 2, -1, -4]))
