84dcff6d54b02f774ff9635450e711f1db387f68
reeli <reeli.null@gmail.com>
2022-01-12 13:29:50 +0800

    update uniquePaths

--- src/dynamic-programming/uniquePaths.ts
+++ src/dynamic-programming/uniquePaths.ts
@@ -10,9 +10,6 @@
 
 export function uniquePaths(m: number, n: number): number {
   const fn = (y: number, x: number, total: number = 0): number => {
-    if (y == 1 && x == 1) {
-      return total + 1;
-    }
     if (y <= 0 || x <= 0) {
       return total;
     }
@@ -28,24 +25,25 @@ export function uniquePaths(m: number, n: number): number {
 export function uniquePaths1(m: number, n: number): number {
   const cache: { [key: string]: number } = {};
 
-  const fn = (y: number, x: number, total: number = 0): number => {
-    if (y == 1 && x == 1) {
-      return total + 1;
+  const fn = (y: number, x: number): number => {
+    if (y < 0 || x < 0) {
+      return 0;
     }
-    if (y <= 0 || x <= 0) {
-      return total;
+
+    if (y == 0 || x == 0) {
+      return 1;
     }
 
     const a1 = `${y - 1},${x}`;
     const a2 = `${y},${x - 1}`;
 
-    cache[a1] = cache[a1] || fn(y - 1, x, total);
-    cache[a2] = cache[a2] || fn(y, x - 1, total);
+    cache[a1] = cache[a1] || fn(y - 1, x);
+    cache[a2] = cache[a2] || fn(y, x - 1);
 
     return cache[a1] + cache[a2];
   };
 
-  return fn(m, n, 0);
+  return fn(m - 1, n - 1);
 }
 
 console.log(uniquePaths1(3, 3));
